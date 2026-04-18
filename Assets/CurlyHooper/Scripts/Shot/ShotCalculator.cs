1d208083d83551c08c240e00544001f7188cb0d0
Brian Tria <brian.tria.se@gmail.com>
2026-04-19 06:04:31 +0800

    better aim

--- Assets/CurlyHooper/Scripts/Shot/ShotCalculator.cs
+++ Assets/CurlyHooper/Scripts/Shot/ShotCalculator.cs
@@ -7,6 +7,7 @@ namespace CurlyHooper
         [SerializeField] private Transform _hoopTarget;
         [SerializeField] private float _baseArcBias = 0.5f;
         [SerializeField] private float _distanceArcMultiplier = 0.1f;
+        [SerializeField, Range(0, 1)] private float _aimAssistStrength = 0.4f;
 
         private Camera _camera;
 
@@ -15,19 +16,32 @@ namespace CurlyHooper
             _camera = Camera.main;
         }
 
-        public Vector3 GetCalculatedShootDirection(Transform playerTransform)
+        public Vector3 GetCalculatedShootDirection(Transform playerTransform, Vector3 ballWorldPos)
         {
+            // 1. Find the point the crosshair is looking at (50 meters away)
+            Ray ray = _camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            Vector3 focalPoint = ray.GetPoint(10f);
+
+            // 2. Calculate direction from the BALL (on the right) to that focal point
+            // This creates the "inward" angle so the ball moves toward the center
+            Vector3 convergenceDir = (focalPoint - ballWorldPos).normalized;
+
             if (_hoopTarget == null)
             {
-                return Camera.main.transform.forward;
+                return convergenceDir;
             }
 
+            // 3. Get horizontal direction to hoop for magnetism
             Vector3 flatPlayerPos = new Vector3(playerTransform.position.x, 0, playerTransform.position.z);
             Vector3 flatHoopPos = new Vector3(_hoopTarget.position.x, 0, _hoopTarget.position.z);
             Vector3 directionToHoop = (flatHoopPos - flatPlayerPos).normalized;
+
+            // 4. Blend the converged direction with the hoop direction
+            Vector3 finalDir = Vector3.Lerp(convergenceDir, directionToHoop, _aimAssistStrength);
+
+            // 5. Add the vertical arc
             float distance = Vector3.Distance(flatPlayerPos, flatHoopPos);
             float dynamicArc = _baseArcBias + (distance * _distanceArcMultiplier);
-            Vector3 finalDir = Vector3.Lerp(_camera.transform.forward, directionToHoop, 0.3f);
             finalDir.y += dynamicArc;
 
             return finalDir.normalized;
